8865ffe008305e44a155fcc8f9d820fb757a92ab
Nagi2880 <alexanderamaral17@gmail.com>
2022-01-03 07:35:01 -0300

    I finished the first web view for the Login part of the web

--- src/components/Header1.jsx
+++ src/components/Header1.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
+
 export default function Header1() {
     return (
         <header id="Header1">
-            <navbar id="navbar-elements" className='d-flex justify-content-center'>
-                <img src="" alt="logo" ></img>
-                <h1>Mateify</h1>
+            <navbar id="navbar-elements" className='d-flex justify-content-center align-items-baseline'>
+                <img id="Img-logo" src='/Logo1.png'alt="logo" ></img>
+                <h1 id="Name-logo">Mateify</h1>
             </navbar>
            <hr className='Header-line' />
         </header>
